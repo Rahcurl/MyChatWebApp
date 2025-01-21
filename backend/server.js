5d2b5473123acf63f2872dd9a61298fa8a1061d3
Rahcurl <rsharma194@rku.ac.in>
2025-01-22 01:22:40 +0530

    passing the Chats details using Id

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,130 @@
 const express = require("express");
 const app = express();
+
+// only passing the Chats details using Id in url
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "John Doe",
+        email: "john@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "John Doe",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Guest User",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anthony",
+        email: "anthony@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anthony",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Friends",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Jane Doe",
+        email: "jane@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Jane Doe",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "John Doe",
+        email: "jon@example.com",
+      },
+      {
+        name: "Piyush",
+        email: "piyush@example.com",
+      },
+      {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Guest User",
+      email: "guest@example.com",
+    },
+  },
+];
+// Server will run at Localhost by this url "localhost:5000"
 app.get("/", (req, res) => {
-  res.send("API is Running");
+  res.send("API is Running"); //for string
 });
+
+//Bunch of dummy data
+app.get("/api/chat", (req, res) => {
+  res.send(chats); //for JSON Dummy data
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat); //for JSON Dummy data
+});
+
 app.listen(5000, console.log("Server started on port 5000")); //app will start on this server having 5000 port number
+
+module.exports = { chats };
